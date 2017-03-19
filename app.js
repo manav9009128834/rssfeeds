691fbd0b9f5c084f7b74425af1a3f62cd05a4060
ashu-tp <ashu@techpenguins.com>
2017-03-19 19:53:15 +0530

    search articles

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ var index = require('./routes/index');
 var users = require('./routes/users');
 var articles = require('./routes/articles');
 var api = require('./routes/api');
+var search = require('./routes/search');
 
 var app = express();
 
@@ -31,6 +32,7 @@ app.use('/', index);
 app.use('/users', users);
 app.use('/articles', articles);
 app.use('/api/v1', api);
+app.use('/search', search);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
