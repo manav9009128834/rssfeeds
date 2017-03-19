fe034a9bc6a3d50a87f33254b9c7c87724ff769b
ashu-tp <ashu@techpenguins.com>
2017-03-19 13:39:07 +0530

    showing articles

---
+++ routes/api.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var router = express.Router();
+var mongodb =  require('../utils/mongodb');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  mongodb(function (err, dbObj) {
+    if (err) {
+      console.log("err in connecting db");
+      res.send('respond with a resource: '+err);
+    } else {
+      console.log("connected db: " + dbObj);
+      mongodb.readarticles(dbObj, 'article','',function (err,data) {
+        if (err) {
+          console.log(err);
+          res.send('respond with a resource: '+err);
+        } else {
+          console.log("data articles :" + JSON.stringify(data));
+          //res.send('respond with a resource: '+JSON.stringify(data));
+          res.render('articles', { data: data, title : 'Articles list'});
+        }
+      });
+    }
+  });
+
+
+});
+
+module.exports = router;
